aa3c94bb23cbdc8307340b63fb5d5351b3bd0f10
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2016-07-14 19:39:17 -0700

    Explicit variable definitions & reverse tape following

--- Main/Movement.ino
+++ Main/Movement.ino
@@ -28,6 +28,14 @@
 #define LEFT 'L'
 #define FORWARD 'F'
 #define UNDEFINED 'U'
+#define LEFT_INTERSECTION 0
+#define RIGHT_INTERSECTION 1
+#define LEFT_TAPE 5
+#define RIGHT_TAPE 3
+#define LEFT_MOTOR 2
+#define RIGHT_MOTOR 1
+#define PROPORTIONAL 7
+#define DERIVATIVE 6
 
 int lthresh = 30; //Threshold before left QRD detects paper 
 int rthresh = 30; //Threshold before right QRD detects paper
@@ -39,7 +47,7 @@ double m = 0; //#Clock oulses in current state, relating to error
 double d; //PID Derivative. m = y/x = (error-lasterr)/(q+m)
 double p; //PID Proportional. 
 double con; //Control Applied = kd*d + kp*p;
-double vel = 60; // Current to motor
+double vel = 40; // Current to motor
 int c = 0; //Counter
 int t = 0; //Counter
 
@@ -62,10 +70,10 @@ int t = 0; //Counter
 */
 void followTape(){ 
 
-  int kd = knob(6)/4; //Derivative Gain Multiplier 
-  int kp = knob(7)/4; //Proportional Gain Multiplier
-  int left = analogRead(5); //Left QRD Signal
-  int right = analogRead(3); //Right QRD Signal
+  int kd = knob(DERIVATIVE)/4; //Derivative Gain Multiplier 
+  int kp = knob(PROPORTIONAL)/4; //Proportional Gain Multiplier
+  int left = analogRead(LEFT_TAPE); //Left QRD Signal
+  int right = analogRead(RIGHT_TAPE); //Right QRD Signal
 
    if ((left>lthresh)&&(right>rthresh)) error = 0;
    if ((left>lthresh)&&(right<rthresh)) error = -1;
@@ -96,7 +104,6 @@ void followTape(){
          LCD.print(analogRead(3)); 
          LCD.print("kd:");
          LCD.print(kd); 
-    
          LCD.setCursor(0,1);
          LCD.print("li:");
          LCD.print(analogRead(0));
@@ -108,8 +115,60 @@ void followTape(){
      }
    c=c+1;
    m=m+1;
-   motor.speed(2,vel-con); //left
-   motor.speed(3,vel+con); //right
+   motor.speed(LEFT_MOTOR,vel-con); //left
+   motor.speed(RIGHT_MOTOR,vel+con); //right
+   lasterr=error;
+}
+
+// TO BE COMPLETED - WRITE DESCRIPTION
+void r_followTape(){
+  int kd = knob(DERIVATIVE)/4; //Derivative Gain Multiplier 
+  int kp = knob(PROPORTIONAL)/4; //Proportional Gain Multiplier
+  int left = analogRead(LEFT_TAPE); //Left QRD Signal
+  int right = analogRead(RIGHT_TAPE); //Right QRD Signal
+
+   if ((left>lthresh)&&(right>rthresh)) error = 0;
+   if ((left>lthresh)&&(right<rthresh)) error = -1;
+   if ((left<lthresh)&&(right>rthresh)) error = +1;
+   if ((left<lthresh)&&(right<rthresh))
+   {
+     if (lasterr>0) error = 5;
+     if (lasterr<=0) error = -5;
+   }
+   if (!(error==lasterr))
+   {
+     recerr=lasterr;
+     q=m;
+     m=1;
+   }
+  
+   p=kp*error;
+   d=(int)((float)kd*(float)(error-recerr)/(float)(q+m));
+   con = p+d;
+
+   if (c==10)
+     {
+         LCD.clear();
+         LCD.setCursor(0,0);
+         LCD.print("lm:");
+         LCD.print(analogRead(5));
+         LCD.print("rm:");
+         LCD.print(analogRead(3)); 
+         LCD.print("kd:");
+         LCD.print(kd); 
+         LCD.setCursor(0,1);
+         LCD.print("li:");
+         LCD.print(analogRead(0));
+         LCD.print("ri:");
+         LCD.print(analogRead(1));
+          LCD.print("kp:");
+         LCD.print(kp); 
+         c=0;
+     }
+   c=c+1;
+   m=m+1;
+   motor.speed(LEFT_MOTOR,-(vel-con)); //left
+   motor.speed(RIGHT_MOTOR,-(vel+con)); //right
    lasterr=error;
 }
 
@@ -129,13 +188,12 @@ void followTape(){
   TINAH Inputs:
     * digital 0: Left QRD intersection
     * digital 1: Right QRD intersection
-
 */
 bool detectIntersection(char dir){
 
   bool output = false;
-  int left = analogRead(0);
-  int right = analogRead(1);
+  int left = analogRead(LEFT_INTERSECTION);
+  int right = analogRead(RIGHT_INTERSECTION);
 
   if (dir == LEFT){
     if (left > lthresh) output = true;
@@ -149,7 +207,6 @@ bool detectIntersection(char dir){
 
   return output;
 }
-
 
 /*
   Function: turn
@@ -165,23 +222,22 @@ bool detectIntersection(char dir){
   TINAH Inputs:
     * Motor 0: Right Motor
     * Motor 1: Left Motor
-
 */
 void turn(char dir){
 
-   int Lm = 0; //Left middle QRD Signal
-   int Rm = 0; //Right middle QRD Signal
+   int L = 0; //Left middle QRD Signal
+   int R = 0; //Right middle QRD Signal
 
    // Begin by going past the intersection. 
    // This will give us space to make a wider turn.
 
    if (dir == LEFT){
       delay(150); //Overshoot
-      motor.speed(2,-50); //left
-      motor.speed(3,50); //right
+      motor.speed(LEFT_MOTOR,-50); //left
+      motor.speed(RIGHT_MOTOR,50); //right
       delay(500);
-      while(Rm < rthresh){;
-        Rm = analogRead(3);
+      while(R < rthresh){;
+        R = analogRead(RIGHT_TAPE);
       }
       motor.stop_all();
       error = 0; //Reset PID
@@ -189,20 +245,59 @@ void turn(char dir){
    } 
    else if (dir == RIGHT){
       delay(150); //Overshoot
-      motor.speed(2,50); //left
-      motor.speed(3,-50); //right
+      motor.speed(LEFT_MOTOR,50); //left
+      motor.speed(RIGHT_MOTOR,-50); //right
       delay(500); //Pause for 0.5s
-      while(Lm < lthresh){
-          Lm = analogRead(5);
+      while(L < lthresh){
+          L = analogRead(LEFT_TAPE);
+      }
+      motor.stop_all();   
+      error = 0; //Reset PID
+      lasterr = 0;
+   } 
+   else{
+      motor.speed(LEFT_MOTOR,20-con); //left
+      motor.speed(RIGHT_MOTOR,20+con); //right
+      delay(180); //Just pass the intersection
+   }
+}
+
+//TO BE COMPLETED - WRITE DESCRIPTION
+void wide_turn(char dir){
+
+   int L = 0; //Left middle QRD Signal
+   int R = 0; //Right middle QRD Signal
+
+   // Begin by going past the intersection. 
+   // This will give us space to make a wider turn.
+
+   if (dir == LEFT){
+      delay(150); //Overshoot
+      motor.speed(LEFT_MOTOR,0); //left
+      motor.speed(RIGHT_MOTOR,50); //right
+      delay(500);
+      while(R < rthresh){;
+        R = analogRead(RIGHT_TAPE);
       }
       motor.stop_all();
       error = 0; //Reset PID
       lasterr = 0;
    } 
+   else if (dir == RIGHT){
+      delay(150); //Overshoot
+      motor.speed(LEFT_MOTOR,50); //left
+      motor.speed(RIGHT_MOTOR,0); //right
+      delay(500); //Pause for 0.5s
+      while(L < lthresh){
+          L = analogRead(LEFT_TAPE);
+      }
+      motor.stop_all();   
+      error = 0; //Reset PID
+      lasterr = 0;
+   } 
    else{
-      motor.speed(2,20-con); //left
-      motor.speed(3,20+con); //right
-      delay(180); //Just pass the intersection
+      //motor.speed(LEFT_MOTOR,20-con); //left
+      //motor.speed(RIGHT_MOTOR,20+con); //right
+      //delay(180); //Just pass the intersection
    }
 }
-
